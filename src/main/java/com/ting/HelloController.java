8383f448b2984d2f5445b8c13a3aa85f08b1823e
Ting <zt_26@qq.com>
2017-05-09 23:07:28 +0800

    request param ,path variable

--- src/main/java/com/ting/HelloController.java
+++ src/main/java/com/ting/HelloController.java
@@ -2,7 +2,9 @@ package com.ting;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,13 +25,32 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String say() {
-//        return "Hello Spring Boot!"+cupSize;
+    //  return "Hello Spring Boot!"+cupSize;
         return content;
     }
 
-    //multi url mapping adapt
+    //    multi url mapping adapt
     @RequestMapping(value = {"/hello2", "/hello3"})
-    public String say2() {
+    public String multiSay() {
         return girlProperties.getAge() + ":" + girlProperties.getCupSize();
     }
+
+/**you can run following examples by ctrl+click on url*/
+
+    // url mapping could be dynamic
+    // example: http://127.0.0.1:8080/spring-boot-demo/say/1
+    // output: id:1
+    @RequestMapping(value = {"/say/{id}"})
+    public String dynamicSay(@PathVariable("id") Integer id) {
+        return "id:" + id;
+    }
+
+    // get request param
+    // example: http://127.0.0.1:8080/spring-boot-demo/say?id=123
+    // output: id:123
+    @RequestMapping(value = {"/say"})
+    public String sayWithParam(@RequestParam("id") Integer id) {
+        return "id:" + id;
+    }
+
 }
